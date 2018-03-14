c0bc745c586b409ac682668248f373b250321b17
Danny Voce <davjr96@gmail.com>
2018-03-14 10:55:44 -0400

    navbar styling

--- front-end/components/App.js
+++ front-end/components/App.js
@@ -5,6 +5,7 @@ import {
   NavLink,
   Link
 } from "react-router-dom";
+import { LinkContainer } from "react-router-bootstrap";
 import { connect } from "react-redux";
 import { logout } from "../actions/user";
 import {
@@ -25,17 +26,42 @@ const ProtectedMap = userIsAuthenticatedRedir(Map);
 const ProtectedTable = userIsAuthenticatedRedir(Table);
 
 const LoginOnlyNav = userIsAuthenticated(({ logout }) => (
-  <nav className="navbar navbar-dark bg-primary">
-    <DateComponent />
-    <Link className="navbar-item btn-light btn" to="/">
-      Map
-    </Link>
-    <Link className="navbar-item btn-light btn" to="/table">
-      Table
-    </Link>
-    <a className="navbar-item btn-light btn" onClick={() => logout()}>
-      Logout
-    </a>
+  <nav className="navbar navbar-expand-lg navbar-light">
+    <LinkContainer to="/">
+      <a className="navbar-brand">VFIS</a>
+    </LinkContainer>{" "}
+    <button
+      className="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span className="navbar-toggler-icon" />
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav mr-auto">
+        <li className="nav-item">
+          <DateComponent />
+        </li>
+        <li className="nav-item">
+          <LinkContainer to="/">
+            <a className=" nav-link">Map</a>
+          </LinkContainer>
+        </li>
+
+        <li className="nav-item">
+          <LinkContainer to="/table">
+            <a className=" nav-link">Table</a>
+          </LinkContainer>
+        </li>
+      </ul>
+      <a className="nav-item nav-link" onClick={() => logout()}>
+        Logout
+      </a>
+    </div>
   </nav>
 ));
 
