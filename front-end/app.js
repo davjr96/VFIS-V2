f94c23829e1c0365471b27cbbdb246b7ffda790c
Danny Voce <davjr96@gmail.com>
2018-03-31 09:11:51 -0400

    Persistant store

--- front-end/app.js
+++ front-end/app.js
@@ -3,11 +3,21 @@ import ReactDOM from "react-dom";
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
 import thunkMiddleware from "redux-thunk";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web and AsyncStorage for react-native
 
 import * as reducers from "./reducers";
 import App from "./components/App";
+import { PersistGate } from "redux-persist/integration/react";
 
 const reducer = combineReducers(Object.assign({}, reducers, {}));
+
+const persistConfig = {
+  key: "root",
+  storage
+};
+
+const persistedReducer = persistReducer(persistConfig, reducer);
 
 const enhancer = compose(
   // Middleware you want to use in development:
@@ -15,13 +25,14 @@ const enhancer = compose(
 );
 
 // Note: passing enhancer as the last argument requires redux@>=3.1.0
-const store = createStore(reducer, enhancer);
+const store = createStore(persistedReducer, enhancer);
+const persistor = persistStore(store);
 
 ReactDOM.render(
   <Provider store={store}>
-    <div>
+    <PersistGate loading={null} persistor={persistor}>
       <App />
-    </div>
+    </PersistGate>
   </Provider>,
   document.getElementById("mount")
 );
