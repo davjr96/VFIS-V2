0ee0aa5d8c219d33c15c9b7727358f58d41ce692
D396 <davjr96@gmail.com>
2018-04-16 00:04:33 -0400

    Color

--- front-end/components/legend.js
+++ front-end/components/legend.js
@@ -6,7 +6,7 @@ const ICON = `M20.2,15.7L20.2,15.7c1.1-1.6,1.8-3.6,1.8-5.7c0-5.6-4.5-10-10-10S2,
 
 const GREEN = "#4cb947";
 const RED = "#ff0000";
-const YELLOW = "#FFA500";
+const YELLOW = "#CCCC00";
 const size = 20;
 var green = {
   cursor: "pointer",
