82df298bedd93fd644c6b3c52d0371885c2de31d
Danny Voce <davjr96@gmail.com>
2018-05-28 12:27:10 -0700

    Remove source map to reduce server memory usage during build

--- front-end/webpack.prod.js
+++ front-end/webpack.prod.js
@@ -4,10 +4,9 @@ const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
 const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
-  devtool: "source-map",
   plugins: [
     new UglifyJSPlugin({
-      sourceMap: true
+      sourceMap: false
     }),
     new webpack.DefinePlugin({
       "process.env.NODE_ENV": JSON.stringify("production")
