b6d568181aae7097a288ef8d178afead813aac35
Danny Voce <davjr96@gmail.com>
2018-05-28 12:36:22 -0700

    Prod build

--- front-end/webpack.prod.js
+++ front-end/webpack.prod.js
@@ -4,9 +4,10 @@ const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
 const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
+  devtool: "source-map",
   plugins: [
     new UglifyJSPlugin({
-      sourceMap: false
+      sourceMap: true
     }),
     new webpack.DefinePlugin({
       "process.env.NODE_ENV": JSON.stringify("production")
